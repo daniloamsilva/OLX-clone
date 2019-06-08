bde9798ad85055e47e030c55428f784d8221be9e
Danilo Augusto <danilo_dams@yahoo.com.br>
2019-06-07 23:56:33 -0300

    Interface de cadastro de anúncio

--- app/src/main/java/com/app/olx/activity/AnunciosActivity.java
+++ app/src/main/java/com/app/olx/activity/AnunciosActivity.java
@@ -49,6 +49,8 @@ public class AnunciosActivity extends AppCompatActivity {
                 autenticacao.signOut();
                 invalidateOptionsMenu();
                 break;
+            case R.id.menu_anuncios:
+                startActivity(new Intent(getApplicationContext(), MeusAnunciosActivity.class));
         }
         return super.onOptionsItemSelected(item);
     }
